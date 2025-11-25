77799944a26da65d2a588c3a081a474256e2116a
Hany <honamagdy2@yahoo.com>
2025-11-25 13:17:07 +0100

    test-Hany

--- HRProject/Models/UserCompetenceProfileViewModel.cs
+++ HRProject/Models/UserCompetenceProfileViewModel.cs
@@ -1,7 +1,7 @@
 ﻿namespace HRProject.Models
 {
     public class UserCompetenceProfileViewModel
-    {
+    {//
         public string UserEmail { get; set; }
         public string FullName { get; set; }
         public string JobTitle { get; set; }
