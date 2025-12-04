28e86a7ba39bfcad39e7a18e6f88d82952272732
erikw <erikw@DESKTOP-D87HANB>
2025-12-04 14:03:16 +0100

    New changes to separate classes

---
+++ HRProject/Models/ProjectRequirement.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRProject.Models
+{
+    public class ProjectRequirement
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+        public ProjectManager Project { get; set; }
+
+        [Required]
+        public int CompetenceId { get; set; }
+        public Competence Competence { get; set; }
+
+        // Minimum level required (1=Basic, 2=Intermediate, 3=Advanced)
+        [Range(1, 3)]
+        public int MinLevel { get; set; }
+
+        // Minimum years of experience
+        [Range(0, 50)]
+        public int MinYearsOfExperience { get; set; }
+    }
+}
