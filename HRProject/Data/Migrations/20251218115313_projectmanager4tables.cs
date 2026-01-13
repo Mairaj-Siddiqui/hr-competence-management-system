d022b0845648800b2da97add0ea16cd5bb58d211
Mairaj Siddiqui <148498576+Mairaj-Siddiqui@users.noreply.github.com>
2026-01-13 13:06:13 +0100

    Commit - HR Skill Gap

---
+++ HRProject/Data/Migrations/20251218115313_projectmanager4tables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRProject.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class projectmanager4tables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
